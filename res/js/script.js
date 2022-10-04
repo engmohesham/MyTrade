425a394780e64f91f8956975dd58b9eb0e62e0e0
Mohamed Hesham <68177072+engmohesham@users.noreply.github.com>
2022-10-04 06:59:55 +0200

    Add files via upload

--- res/js/script.js
+++ res/js/script.js
@@ -14,21 +14,25 @@ window.onscroll = function () {
 // Get the header
 var header = document.getElementById("fixHead");
 var container = document.querySelector('.container'),
-link = document.querySelectorAll('.link');
+link = document.querySelectorAll('.link'),
+scrollIcon = document.querySelector('.scroll'),
+department = document.querySelector('.depart');
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+var sticky = header.offsetTop,
+stickyBig = header.offsetHeight;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+    header.classList.add("sticky-top");
+    scrollIcon.style.display = "none";
     container.style.boxShadow = "0px 0px 15px -5px rgba(70, 70, 70, 0.5)";
     link[1].style.color = "#fff";
     link[2].style.color = "#fff";
-    
   } else {
     header.classList.remove("sticky");
+    scrollIcon.style.display = "block";
     container.style.boxShadow = "0px 0px 15px 0px rgba(0, 0, 0, 0.7)";
     link[1].style.color = "#969696";
     link[2].style.color = "#969696";
@@ -42,7 +46,35 @@ $(document).ready(function(){
     arrows: false,
     autoplay: true,
     autoplaySpeed: 2000,
-    fade: true,
-    dots: true
+    fade: true
   });
 });
+
+$(document).ready(function(){
+  $(".de-slide").slick({
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    infinite: true
+  });
+});
+
+scrollIcon.addEventListener('click', function(){
+  var ele = document.getElementById('department');
+  window.scrollTo(ele.offsetLeft, ele.offsetTop);
+})
+
+window.onscroll = function(){
+  if(window.pageYOffset > stickyBig){
+    department.classList.add('scrolled'),
+    department.classList.remove('js-scroll');
+  }else{
+    department.classList.remove('scrolled'),
+    department.classList.add('js-scroll');
+  }
+}
+
+
+
